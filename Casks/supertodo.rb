e929a36824682365d41738ead1533793dc0e6d95
andreasd <andreas@limina.com>
2026-08-18 18:37:39 +0200

    Adopt arch idiom, add macOS floor (brew style --fix)

--- Casks/supertodo.rb
+++ Casks/supertodo.rb
@@ -1,15 +1,11 @@
 cask "supertodo" do
+  arch arm: "-arm64"
+
   version "0.1.1"
+  sha256 arm:   "f2d305eeff2483934b21562eeac740775cc4009b3dd9f4ded1ad06d0b050e137",
+         intel: "b23b6d5e0a3c11c406822882773e5bb2b4155e3215c37163bf1f8a26ccc3234f"
 
-  on_arm do
-    sha256 "f2d305eeff2483934b21562eeac740775cc4009b3dd9f4ded1ad06d0b050e137"
-    url "https://github.com/furst/supertodo/releases/download/v#{version}/supertodo-#{version}-arm64.dmg"
-  end
-  on_intel do
-    sha256 "b23b6d5e0a3c11c406822882773e5bb2b4155e3215c37163bf1f8a26ccc3234f"
-    url "https://github.com/furst/supertodo/releases/download/v#{version}/supertodo-#{version}.dmg"
-  end
-
+  url "https://github.com/furst/supertodo/releases/download/v#{version}/supertodo-#{version}#{arch}.dmg"
   name "supertodo"
   desc "Desktop shell for the supertodo app"
   homepage "https://github.com/furst/supertodo"
@@ -18,6 +14,8 @@ cask "supertodo" do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: :big_sur
 
   app "supertodo.app"
 
