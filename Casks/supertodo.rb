d45bccef742fc812882ff96d2c277346a00de2cd
andreasd <andreas@limina.com>
2026-08-19 08:50:42 +0200

    Serve DMGs from this repo's releases (source repo is private)

--- Casks/supertodo.rb
+++ Casks/supertodo.rb
@@ -5,10 +5,10 @@ cask "supertodo" do
   sha256 arm:   "b6fc7a6a91b09411461fd530b1184dd90a940ee9753e9cb3b8561aa13218f22c",
          intel: "7890abf705ec0cfd76837e3437db81f11b29e12f4df9bac331e9179a60ae1260"
 
-  url "https://github.com/furst/supertodo/releases/download/v#{version}/supertodo-#{version}#{arch}.dmg"
+  url "https://github.com/fursted/homebrew-tap/releases/download/v#{version}/supertodo-#{version}#{arch}.dmg"
   name "supertodo"
   desc "Desktop shell for the supertodo app"
-  homepage "https://github.com/furst/supertodo"
+  homepage "https://github.com/fursted/homebrew-tap"
 
   livecheck do
     url :url
